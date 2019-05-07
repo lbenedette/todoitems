cfb20b47b83cf8260d614d204b51335f062d1f77
Leonardo Lopes <blopesleo@outlook.com>
2019-05-06 23:49:21 -0300

    Added task resources, routes modifications and namespace api created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace 'api' do
+    get '/tasks' => 'tasks#index'
+    post '/tasks' => 'tasks#create'
+    get '/tasks/:task_id' => 'tasks#show'
+    post '/tasks/:task_id/update' => 'tasks#update'
+    post '/tasks/:task_id/delete' => 'tasks#delete'
+    post '/tasks/:task_id/completed' => 'tasks#completed'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
